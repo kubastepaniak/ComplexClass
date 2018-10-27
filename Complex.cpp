ea0bcebbe7acd1c7edbc4b9a8e59cc10d5202a44
Jakub Stepaniak <stepaniak.kuba@gmail.com>
2018-10-27 21:41:53 +0200

    saturday's update

--- Complex.cpp
+++ Complex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Complex.h"
 
 Complex::Complex()
 {
@@ -10,20 +11,42 @@ Complex::~Complex() {}
 
 Complex::Complex(double re, double im)
 {
-	this.re = re;
-	this.im = im;
+	this->re = re;
+	this->im = im;
 }
 
-//Complex operator+(Complex & a, Complex & b);
-//Complex operator-(Complex & a, Complex & b);
-//Complex operator*(Complex & a, Complex & b);
-//Complex operator/(Complex & a, Complex & b);
+double Complex::getReal() {	return re; }
+
+double Complex::getImag() {	return im; }
+
+Complex Complex::operator+(Complex & b)
+{
+	return (Complex(re + b.getReal(), im + b.getImag()));
+}
+
+Complex Complex::operator-(Complex & b)
+{
+	return (Complex(re - b.getReal(), im - b.getImag()));
+}
+
+Complex Complex::operator*(Complex & b)
+{
+	return (Complex(re * b.getReal() + (-1)*(im)*(b.getImag()), 
+					im * b.getReal() + re * b.getImag()));
+}
+
+Complex Complex::operator/(Complex & b)
+{
+	return (Complex((re * b.getReal() + im * b.getImag()) / 
+					(b.getReal() * b.getReal() + b.getImag() * b.getImag()),
+					(im * b.getReal() - re * b.getImag()) / 
+					(b.getReal() * b.getReal() + b.getImag() * b.getImag())));
+}
+
+//double Amp();
+//double Phase();
 //Complex operator+=(Complex & a, Complex & b);
 //Complex operator-=(Complex & a, Complex & b);
 //Complex operator*=(Complex & a, Complex & b);
 //Complex operator/=(Complex & a, Complex & b);
 //bool operator==(Complex & a, Complex & b);
-//double Re();
-//double Im();
-//double Amp();
-//double Phase();
--- TestComplex.cpp
+++ TestComplex.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+#include "Complex.h"
 using namespace std;
 
 int main()
 {
+	Complex a(5, 10);
+	Complex b(3, -7);
+	Complex c;
+
+	//create cout display function
+	//cout << "a: " << 
+
+	c = a + b;
+	cout << "addition: " << c.getReal() << ", " << c.getImag() << endl;
+
+	c = a - b;
+	cout << "subtraction: " << c.getReal() << ", " << c.getImag() << endl;
+
+	c = a * b;
+	cout << "multiplication: " << c.getReal() << ", " << c.getImag() << endl;
+
+	c = a / b;
+	cout << "division: " << c.getReal() << ", " << c.getImag() << endl;
+
 	return 0;
 }
